2a31067e78cf7bf680711ed589eb3ef52926398f
lajos papp <lalyos@yahoo.com>
2012-07-19 18:51:38 +0200

    Memory based OrderService is added

---
+++ src/main/java/com/epam/junior/service/simple/SimpleOrderService.java
@@ -0,0 +1,46 @@
+package com.epam.junior.service.simple;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.epam.junior.domain.Order;
+import com.epam.junior.domain.OrderNotFountException;
+import com.epam.junior.domain.OrderState;
+import com.epam.junior.service.OrderService;
+
+public class SimpleOrderService implements OrderService {
+
+    private static Integer orderId = 0;
+    private Map<String, Order> orders = new HashMap<String, Order>();
+    
+    public String doOrder(Order order) {
+        String newId =  "" + orderId++;
+        order.setId(newId);
+        orders.put(newId, order);
+        return newId;
+    }
+    
+    public OrderState getState(String orderId) {
+        Order order = orders.get(orderId);
+        if (order == null) {
+            throw new OrderNotFountException(orderId); 
+        }
+        return order.getState();
+    }
+    
+    public List<Order> getFullHistory() {
+        return new ArrayList<Order>(orders.values());
+    }
+    public List<Order> getCustomerHistory(String customer) {
+       List<Order> history = new ArrayList<Order>();
+        for (Order order : orders.values()) {
+            if (order.getCustomer().equals(customer)) {
+                history.add(order);
+            }
+        }
+        return history;
+    }
+}
