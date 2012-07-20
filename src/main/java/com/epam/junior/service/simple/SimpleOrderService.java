e398e44e88062cf9bc2ca81d144fd0bc0f9a31b0
lajos papp <lalyos@yahoo.com>
2012-07-20 11:59:41 +0200

    logging added + typo fixed

--- src/main/java/com/epam/junior/service/simple/SimpleOrderService.java
+++ src/main/java/com/epam/junior/service/simple/SimpleOrderService.java
@@ -6,6 +6,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.epam.junior.App;
 import com.epam.junior.domain.Order;
 import com.epam.junior.domain.OrderNotFountException;
 import com.epam.junior.domain.OrderState;
@@ -15,6 +19,7 @@ public class SimpleOrderService implements OrderService, SimpleOrderServiceMBean
 
     private static Integer orderId = 0;
     private Map<String, Order> orders = new HashMap<String, Order>();
+    private static Logger logger = LoggerFactory.getLogger(SimpleOrderService.class);
     
     public String doOrder(Order order) {
         String newId =  "" + orderId++;
@@ -49,10 +54,16 @@ public class SimpleOrderService implements OrderService, SimpleOrderServiceMBean
         return getState(orderId).name();
     }
 
-    public void changeOrderState(String ordrId, String state) {
+    public void changeOrderState(String orderId, String state) {
+        
         Order order = orders.get(orderId);
+        logger.info("about to change order state {} to {}", orderId, state);
         if (order != null) {
+            logger.info("order found");
             order.setState(OrderState.valueOf(state));
+        }
+        else {
+            logger.warn("order found");            
         }
     }
 }
